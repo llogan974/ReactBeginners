422a5dc9b104b92f5d153768b8f7c2940f3d347d
Luke Logan <llogan@highpoint.edu>
2019-07-23 06:28:38 -0400

    adds notes about componenets

---
+++ catch-of-the-day/notes/components.js
@@ -0,0 +1,16 @@
+/*
+What is a componenet?
+They are part of your webpage.
+
+They allow you to build your own tags.
+
+If you look at dev tools under "react" you will see all the components that react is creating
+
+Each component exists all by itself.
+
+All pieces of an application exist as their own component.
+
+The "App" component has all of the fish data
+
+If you change the state, the info in that component will ALL be updated.
+*/
